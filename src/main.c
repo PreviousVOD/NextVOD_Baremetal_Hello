f28dda75420fd4a92934613135e920f75fe2637f
imi415 <imi415@imi.moe>
2022-06-25 16:29:45 +0800

    We have main.

--- src/main.c
+++ src/main.c
@@ -14,8 +14,22 @@
 #define ST_GPIO0_OFFSET_SET_PC2  0x44U
 #define ST_GPIO0_OFFSET_CLR_PC2  0x48U
 
-#define LED_RED_PIN  4U
-#define LED_BLUE_PIN 5U
+#define SH_TMU_BASE_ADDR    (0xFFD80000)
+#define SH_TMU_OFFSET_TOCR  0x00U
+#define SH_TMU_OFFSET_TSTR  0x04U
+#define SH_TMU_OFFSET_TCOR0 0x08U
+#define SH_TMU_OFFSET_TCNT0 0x0CU
+#define SH_TMU_OFFSET_TCR0  0x10U
+#define SH_TMU_OFFSET_TCOR1 0x14U
+#define SH_TMU_OFFSET_TCNT1 0x18U
+#define SH_TMU_OFFSET_TCR1  0x1CU
+#define SH_TMU_OFFSET_TCOR2 0x20U
+#define SH_TMU_OFFSET_TCNT2 0x24U
+#define SH_TMU_OFFSET_TCR2  0x28U
+#define SH_TMU_OFFSET_TCPR2 0x2CU
+
+#define LED_RED_PIN  5U
+#define LED_BLUE_PIN 4U
 
 static void init_led(uint8_t pin) {
     *(uint32_t *)(ST_GPIO0_BASE_ADDR + ST_GPIO0_OFFSET_CLR_PC0) = (1 << pin);
@@ -32,27 +46,37 @@ static void set_led(uint8_t pin, uint8_t val) {
     }
 }
 
-static void delay(uint32_t msec) {
-    volatile uint32_t loop = 0;
-    for (uint32_t i = 0; i < msec; i++) {
-        for (uint32_t j = 0; j < 40000; i++) {
-            loop++;
-        }
-    }
+static void delay_ms(uint32_t msec) {
+    /* Initialize TMU and count to zero */
+    /* TMU clock is from Peripheral clock, approx. 66MHz */
+    /* Prescale to 450kHz for convenience (TMUs can only divide by max. 1024) */
+
+    *(uint8_t *)(SH_TMU_BASE_ADDR + SH_TMU_OFFSET_TSTR) &= ~1U;          /* Stop counter */
+    *(uint16_t *)(SH_TMU_BASE_ADDR + SH_TMU_OFFSET_TCR0)  = 0x04U;       /* 1024 prescale */
+    *(uint32_t *)(SH_TMU_BASE_ADDR + SH_TMU_OFFSET_TCNT0) = (msec * 66); /* 66kHz */
+    *(uint8_t *)(SH_TMU_BASE_ADDR + SH_TMU_OFFSET_TSTR) |= 1U;           /* Start counter */
+
+    /* Wait until underflow occurs */
+    uint16_t tcr0 = 0U;
+    do {
+        tcr0 = *(uint16_t *)(SH_TMU_BASE_ADDR + SH_TMU_OFFSET_TCR0);
+    } while ((tcr0 & 0x100) == 0);
+
+    *(uint8_t *)(SH_TMU_BASE_ADDR + SH_TMU_OFFSET_TSTR) &= ~1U; /* Stop counter */
 }
 
 int main(void) {
     init_led(LED_RED_PIN);
     init_led(LED_BLUE_PIN);
 
-    set_led(LED_RED_PIN, 1);
-    set_led(LED_BLUE_PIN, 1);
+    set_led(LED_RED_PIN, 0);
+    set_led(LED_BLUE_PIN, 0);
 
     for (;;) {
         /* Dead loop */
         set_led(LED_BLUE_PIN, 1);
-        delay(1000);
+        delay_ms(1);
         set_led(LED_BLUE_PIN, 0);
-        delay(1000);
+        delay_ms(1);
     }
 }
